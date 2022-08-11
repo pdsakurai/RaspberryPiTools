3755a64c386baea4131a6d15c1791306aed5ec4a
Paul Mikki Sakurai <23254804+pdsakurai@users.noreply.github.com>
2022-08-11 19:40:41 +0800

    Fixed: Unrecognized multi-line comments

--- completed_torrent_handlers/PSA.sh
+++ completed_torrent_handlers/PSA.sh
@@ -25,10 +25,10 @@ function clean_text_using_sed() {
     local text="${1:?Missing: text to clean}"
     local -r custom_sed_options_sequence=( "${!2}" )
 
-    '''Common sed options sequence remarks
+    : 'Common sed options sequence remarks
     Line 1 - removal of tags text, starting from resolution tag going to the right
     Line 2 - enclosing of year released with pair of parentheses
-    Line 3 - changing dot separator to whitespace '''
+    Line 3 - changing dot separator to whitespace'
     local -r common_sed_options_sequence=( \
         "s/.\(216\|108\|72\)0p.*$//" \
         "s/(\?\([0-9]\{4\}\))\?\(.*$\)/\(\1)\2/" \
@@ -43,11 +43,11 @@ function clean_text_using_sed() {
 }
 
 function process_movie() {
-    '''Expected structure:
+    : 'Expected structure:
     Folder: Movie.2022.EXTENDED.1080p.*HEVC-PSA
     Inside it:
         Movie.2022.EXTENDED.1080p.*HEVC-PSA.mkv
-        .*other irrelevant files.*'''
+        .*other irrelevant files.*'
     local -r torrent_path="${1%/}"
     local -r destination="${2:?Missing: Destination}"
 
@@ -71,19 +71,19 @@ function process_tvshow(){
     local -r torrent_path="${1%/}"
     local -r destination="${2:?Missing: Destination}"
 
-    '''Expected structure #1:
-    TV.Show.2022.S01E01.Title.of.episode.720p.+*HEVC-PSA.mkv'''
+    : 'Expected structure #1:
+    TV.Show.2022.S01E01.Title.of.episode.720p.+*HEVC-PSA.mkv'
     if [[ -f "$torrent_path" ]]; then
         local -r file_name_cleaned="$( clean_text_using_sed "${torrent_path##*/}" )"
         local -r cleaned_torrent_path="$( rename "$torrent_path" "$file_name_cleaned" )"
         local -r tv_show_title="$( printf "${cleaned_torrent_path##*/}" | sed "s/.S[0-9]\+E[0-9]\+.*//" )"
         move "$cleaned_torrent_path" "$destination/$tv_show_title"
 
-    '''Expected structure #2:
+    : 'Expected structure #2:
     Folder: TV.Show.2022.SEASON.01.S01.COMPLETE.720p.+HEVC-PSA
     Inside it:
         TV.Show.2022.S01E01.Title.of.episode.720p.+*HEVC-PSA.mkv
-        TV.Show.2022.S01E02.Title.of.episode.720p.+*HEVC-PSA.mkv'''
+        TV.Show.2022.S01E02.Title.of.episode.720p.+*HEVC-PSA.mkv'
     elif [[ -d "$torrent_path" ]]; then
         local -r tv_show_title="$( printf "${torrent_path##*/}" | sed "s/\.SEASON\..\+//" )"
 
