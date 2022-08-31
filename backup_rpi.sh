1091a1cae14af58f9ce8a5a1d50c65f1c57969de
Paul Mikki Sakurai <23254804+pdsakurai@users.noreply.github.com>
2022-08-31 18:43:15 +0800

    Change: abort when daily backup failed Change: Changed naming convention

--- backup_rpi.sh
+++ backup_rpi.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-readonly root_directory="/mnt/eHDD/Software/Devices/Raspberry Pi 3 B+"
-readonly snapshot_directory="$root_directory/Snapshots"
-readonly script_backup="$root_directory/bkup_rpimage/bkup_rpimage.sh"
-readonly script_shrink="$root_directory/PiShrink/pishrink.sh"
-readonly daily_backup_fullfilepath="$root_directory/rpi_backup.img"
+readonly directory_root="/mnt/eHDD/Software/Devices/Raspberry Pi 3 B+"
+readonly directory_snapshot="$directory_root/Snapshots"
+readonly script_backup="$directory_root/bkup_rpimage/bkup_rpimage.sh"
+readonly script_shrink="$directory_root/PiShrink/pishrink.sh"
+readonly fullfilepath_backup="$directory_root/rpi_backup.img"
+readonly fullfilepath_log="$directory_root/log.txt"
 
 readonly debug_mode=
 readonly forced_shrink=
-readonly log_file="$root_directory/log.txt"
-
 
 function log() {
     local -r tag="backup_rpi.sh[$$]"
@@ -18,9 +17,9 @@ function log() {
     logger -t "$tag" "$text"
 
     text="<$time_now> $tag: $text\n"
-    [[ -e "$log_file" ]] \
-        && sed -i "1 s|^|$text|" "$log_file" \
-        || printf "$text" > "$log_file"
+    [[ -e "$fullfilepath_log" ]] \
+        && sed -i "1 s|^|$text|" "$fullfilepath_log" \
+        || printf "$text" > "$fullfilepath_log"
 }
 
 function print_elapsed_time() {
@@ -77,58 +76,62 @@ function byte_to_gigabyte() {
     printf "%d.%02d" $whole_number $fractional_number
 }
 
-function do_daily_backup() {
+function create_daily_backup() {
     SECONDS=0
-    [[ -z $debug_mode ]] && sudo bash "$script_backup" start -c "$daily_backup_fullfilepath"
-
+    [[ -z $debug_mode ]] && sudo bash "$script_backup" start -c "$fullfilepath_backup"
     local -r elapsed_time="$( print_elapsed_time )"
-    local file_size="$( stat -c%s "$daily_backup_fullfilepath" )"
+
+    if [[ ! -f "$fullfilepath_backup" ]]; then
+        log "Failed to create a backup @ \"${fullfilepath_backup}\"."
+        exit 1
+    fi
+    local file_size="$( stat -c%s "$fullfilepath_backup" )"
     file_size="$( byte_to_gigabyte "$file_size" )"
 
-    log "Completed the ${file_size}GB backup within $elapsed_time @ \"${daily_backup_fullfilepath}\"."
+    log "Completed the ${file_size}GB backup within $elapsed_time @ \"${fullfilepath_backup}\"."
 }
 
 function create_snapshot() {
-    local snapshot_filename="${daily_backup_fullfilepath%.img}_$(date +%Y-%m-%d).img"
-    local snapshot_fullfilepath="$snapshot_directory/$snapshot_filename"
+    local snapshot_filename="${fullfilepath_backup%.*}_$(date +%Y-%m-%d).img"
+    local snapshot_fullfilepath="$directory_snapshot/$snapshot_filename"
 
     if [[ $(date +%d) == "01" || $(date +%d) == "16" || -n "$forced_shrink" ]] \
-        && [[ $( ls -1A "$snapshot_directory" | grep -c "$snapshot_filename" 2> /dev/null ) -eq 0 ]]; then
+        && [[ $( ls -1A "$directory_snapshot" | grep -c "$snapshot_filename" 2> /dev/null ) -eq 0 ]]; then
         SECONDS=0
-        [[ -z $debug_mode ]] && sudo bash "$script_shrink" -Z "$daily_backup_fullfilepath" "$snapshot_fullfilepath"
+        [[ -z $debug_mode ]] && sudo bash "$script_shrink" -Z "$fullfilepath_backup" "$snapshot_fullfilepath"
         local -r elapsed_time="$( print_elapsed_time )"
 
-        snapshot_filename="$( ls -1A "$snapshot_directory" | grep "$snapshot_filename" 2> /dev/null )"
-        snapshot_fullfilepath="$snapshot_directory/$snapshot_filename"
+        snapshot_filename="$( ls -1A "$directory_snapshot" | grep "$snapshot_filename" 2> /dev/null )"
+        snapshot_fullfilepath="$directory_snapshot/$snapshot_filename"
         if [[ -z "$snapshot_filename" || ! -f "$snapshot_fullfilepath" ]]; then
-            log "Failed to create snapshot @ \"${snapshot_fullfilepath}\" based on \"${daily_backup_fullfilepath}\"."
+            log "Failed to create a snapshot @ \"${snapshot_fullfilepath}\" based on \"${fullfilepath_backup}\"."
             exit 1
         fi
     
-        local snapshot_filesize="$( stat -c%s "$snapshot_fullfilepath" )"
-        local delta="$( stat -c%s "$daily_backup_fullfilepath" )"
-        delta="$( get_delta "$snapshot_filesize" "$delta" )"
-        snapshot_filesize="$( byte_to_gigabyte "$snapshot_filesize" )"
+        local filesize="$( stat -c%s "$snapshot_fullfilepath" )"
+        local delta="$( stat -c%s "$fullfilepath_backup" )"
+        delta="$( get_delta "$filesize" "$delta" )"
+        filesize="$( byte_to_gigabyte "$filesize" )"
 
-        log "Created the snapshot and shrank it by ${delta}% to ${snapshot_filesize}GB within ${elapsed_time} @ \"${snapshot_fullfilepath}\"."
+        log "Created the snapshot and shrank it by ${delta}% to ${filesize}GB within ${elapsed_time} @ \"${snapshot_fullfilepath}\"."
     fi
 }
 
 function truncate_snapshots() {
-    local -r snapshots_count=$( ls -1A "$snapshot_directory" | grep -c "rpi_backup_" )
+    local -r snapshots_count="$( ls -1A "$directory_snapshot" | grep -c "rpi_backup_" )"
     local -r max_snapshots_count="12"
 
     if [[ $snapshots_count -gt $max_snapshots_count ]]; then
         local -r snapshots_to_delete_count="$(( snapshots_count - max_snapshots_count ))"
 
-        ls -1AX "$snapshot_directory" | head -$snapshots_to_delete_count | while read item; do
-            local old_snapshot="$snapshot_directory/$item"
+        ls -1AX "$directory_snapshot" | head -$snapshots_to_delete_count | while read item; do
+            local old_snapshot="$directory_snapshot/$item"
             [[ -z $debug_mode ]] && rm "$old_snapshot"
             log "Removed old snapshot: \"$old_snapshot\""
         done
     fi
 }
 
-do_daily_backup
+create_daily_backup
 create_snapshot
 truncate_snapshots
