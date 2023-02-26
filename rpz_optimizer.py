4bc1909ab5f87f11bd6699ef9c64b05c2d8db02d
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2023-02-26 10:32:45 +0800

    Refactor: Grouped all zonefile generators into a class

--- rpz_optimizer.py
+++ rpz_optimizer.py
@@ -6,28 +6,44 @@ import tempfile
 import os
 import shutil
 
-def generate_zone_file_header(source):
-    yield f"; Source: {source}\n"
+class ZoneFile:
 
-    today = datetime.now(timezone(timedelta(hours=8))).replace(microsecond=0)
-    yield f"; Last modified: {today.isoformat()}\n\n"
+    def __init__(self, source_url):
+        self.source_url = source_url
+    
+    def _generate_header(self):
+        yield f"; Source: {self.source_url}\n"
 
-    time_to_ = {
-        "refresh" : int(timedelta(days=1).total_seconds()),
-        "retry" : int(timedelta(minutes=1).total_seconds()),
-        "expire" : int(timedelta(days=30).total_seconds()),
-        "expire NXDOMAIN cache": int(timedelta(seconds=30).total_seconds()),
-        "expire SOA": int(timedelta(hours=1).total_seconds()),
-    }
+        today = datetime.now(timezone(timedelta(hours=8))).replace(microsecond=0)
+        yield f"; Last modified: {today.isoformat()}\n\n"
 
-    yield f"$TTL {time_to_['expire SOA']}\n"
-    yield f"@ IN SOA storage.capri. pdsakurai/.gmail.com. (\n"
-    yield f"         {today.strftime('%y%m%d%H%M')}\n"
-    yield f"         {time_to_['refresh']}\n"
-    yield f"         {time_to_['retry']}\n"
-    yield f"         {time_to_['expire']}\n"
-    yield f"         {time_to_['expire NXDOMAIN cache']} )\n"
-    yield f" NS localhost.\n\n"
+        time_to_ = {
+            "refresh" : int(timedelta(days=1).total_seconds()),
+            "retry" : int(timedelta(minutes=1).total_seconds()),
+            "expire" : int(timedelta(days=30).total_seconds()),
+            "expire NXDOMAIN cache": int(timedelta(seconds=30).total_seconds()),
+            "expire SOA": int(timedelta(hours=1).total_seconds()),
+        }
+
+        yield f"$TTL {time_to_['expire SOA']}\n"
+        yield f"@ IN SOA storage.capri. pdsakurai/.gmail.com. (\n"
+        yield f"         {today.strftime('%y%m%d%H%M')}\n"
+        yield f"         {time_to_['refresh']}\n"
+        yield f"         {time_to_['retry']}\n"
+        yield f"         {time_to_['expire']}\n"
+        yield f"         {time_to_['expire NXDOMAIN cache']} )\n"
+        yield f" NS localhost.\n\n"
+
+    def _generate_rpz_rules(self):
+        with request.urlopen(self.source_url) as src_file:
+            regex = re.compile(r"^\w")
+            is_valid_rpz_trigger_rule = lambda entry : regex.match(entry)
+            decoded_lines = (line_in_binary.decode() for line_in_binary in src_file)
+            yield from (line for line in decoded_lines if is_valid_rpz_trigger_rule(line))
+
+    def generate(self):
+        yield from self._generate_header()
+        yield from self._generate_rpz_rules()
 
 def get_arguments():
     arg_parser = ArgumentParser()
@@ -36,19 +52,11 @@ def get_arguments():
     args = arg_parser.parse_args()
     return (args.source_url, args.destination_file)
 
-def generate_zone_file(source_url):
-    yield from generate_zone_file_header(source_url)
-    with request.urlopen(source_url) as src_file:
-        regex = re.compile(r"^\w")
-        is_valid_rpz_trigger_rule = lambda entry : regex.match(entry)
-        decoded_lines = (line_in_binary.decode() for line_in_binary in src_file)
-        yield from (line for line in decoded_lines if is_valid_rpz_trigger_rule(line))
-
 source_url, destination_file=get_arguments()
 with tempfile.TemporaryDirectory() as temp_dir:
     temp_file_fd, temp_file_path = tempfile.mkstemp(text=True, dir=temp_dir)
     with os.fdopen(temp_file_fd, mode="w") as temp_file:
-        temp_file.writelines(generate_zone_file(source_url))
+        temp_file.writelines(ZoneFile(source_url).generate())
     shutil.move(temp_file_path, destination_file)
 
 # def get_domain_name():
