abc15ac175fef8bd656fe44a3fe1b63d9f590281
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2023-02-26 09:38:18 +0800

    New: Overwrite destination only after complete processing

--- rpz_optimizer.py
+++ rpz_optimizer.py
@@ -2,6 +2,9 @@ import re
 from urllib import request
 from datetime import datetime, timedelta, timezone
 from argparse import ArgumentParser
+import tempfile
+import os
+import shutil
 
 def generate_zone_file_header(source):
     yield f"; Source: {source}\n"
@@ -35,15 +38,20 @@ source_url=args.source_url[0]
 destination_file=args.destination_file[0]
 
 with request.urlopen(source_url) as src_file:
-    with open(destination_file, mode="w") as dest_file:
-        dest_file.writelines(generate_zone_file_header(source_url))
+    with tempfile.TemporaryDirectory() as temp_dir:
+        
+        temp_file_fd, temp_file_path = tempfile.mkstemp(text=True, dir=temp_dir)
+        with os.fdopen(temp_file_fd, mode="w") as temp_file:
+            temp_file.writelines(generate_zone_file_header(source_url))
 
-        decoded_lines = (line_in_binary.decode() for line_in_binary in src_file)
-        def is_valid_rpz_trigger_rule():
-            regex = re.compile(r"^\w")
-            return lambda entry : regex.match(entry)
-        is_valid_rpz_trigger_rule = is_valid_rpz_trigger_rule()
-        dest_file.writelines(line for line in decoded_lines if is_valid_rpz_trigger_rule(line))
+            decoded_lines = (line_in_binary.decode() for line_in_binary in src_file)
+            def is_valid_rpz_trigger_rule():
+                regex = re.compile(r"^\w")
+                return lambda entry : regex.match(entry)
+            is_valid_rpz_trigger_rule = is_valid_rpz_trigger_rule()
+            temp_file.writelines(line for line in decoded_lines if is_valid_rpz_trigger_rule(line))
+
+        shutil.move(temp_file_path, destination_file)
 
 # def get_domain_name():
 #     regex = re.compile(r"^(?P<domain>(?=\w|\*\.).+)(\s+CNAME\s+\.)", re.IGNORECASE)
