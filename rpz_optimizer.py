c5084550d4babc1ffd039f71f534898bdda3127e
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2023-03-02 14:17:21 +0800

    Refactor: Formatted using black

--- rpz_optimizer.py
+++ rpz_optimizer.py
@@ -14,14 +14,20 @@ def get_arguments():
     arg_parser.add_argument("-d", "--destination_file", required=True, type=str)
     arg_parser.add_argument("-n", "--name_server", required=True, type=str)
     arg_parser.add_argument("-e", "--email_address", required=True, type=str)
-    arg_parser.add_argument("-t", "--type", type=str, choices=["host", "rpz non-wildcards only", "rpz wildcards only"], required=True)
+    arg_parser.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        choices=["host", "rpz non-wildcards only", "rpz wildcards only"],
+        required=True,
+    )
     args = arg_parser.parse_args()
     return (
         args.source_url,
         args.destination_file,
         args.name_server,
         args.email_address,
-        args.type
+        args.type,
     )
 
 
@@ -58,41 +64,47 @@ def header_generator(
 
     yield ""
 
-def extract_domain_name(type_flag : str, next_cb : typing.Coroutine) -> typing.Coroutine:
+
+def extract_domain_name(type_flag: str, next_cb: typing.Coroutine) -> typing.Coroutine:
     def create_domain_name_pattern() -> re.Pattern:
         if type_flag == "host":
             return re.compile(r"^0.0.0.0\s(?P<domain_name>.+)")
         if type_flag == "rpz non-wildcards only":
-            return re.compile(r"^(?P<domain_name>(?=\w).+)(\s+CNAME\s+\.)", re.IGNORECASE)
+            return re.compile(
+                r"^(?P<domain_name>(?=\w).+)(\s+CNAME\s+\.)", re.IGNORECASE
+            )
         if type_flag == "rpz wildcards only":
-            return re.compile(r"^(?P<domain_name>(?=\*\.).+)(\s+CNAME\s+\.)", re.IGNORECASE)
-        raise Exception("Invalid type_flag") 
+            return re.compile(
+                r"^(?P<domain_name>(?=\*\.).+)(\s+CNAME\s+\.)", re.IGNORECASE
+            )
+        raise Exception("Invalid type_flag")
 
-    print (f"Extracting domain names using this format: {type_flag}")
+    print(f"Extracting domain names using this format: {type_flag}")
     domain_name_pattern = create_domain_name_pattern()
     domain_names_extracted = 0
     try:
         while True:
             matches = domain_name_pattern.match((yield))
             if matches:
-                domain_names_extracted+=1
+                domain_names_extracted += 1
                 next_cb.send(matches["domain_name"])
     except GeneratorExit:
-        print (f"Domain names extracted: {domain_names_extracted}")
+        print(f"Domain names extracted: {domain_names_extracted}")
 
 
 def rpz_entry_formatter(next_cb: typing.Coroutine) -> typing.Coroutine:
-    print ("Formatting domain names to NX RPZ-compliant style: <domain name> CNAME .")
+    print("Formatting domain names to NX RPZ-compliant style: <domain name> CNAME .")
     try:
         while True:
             next_cb.send(f"{yield} CNAME .")
     except GeneratorExit:
         pass
 
+
 def writer(destination_file: str):
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_file_fd, temp_file_path = tempfile.mkstemp(dir=temp_dir)
-        print (f"Temporary file created: {temp_file_path}")
+        print(f"Temporary file created: {temp_file_path}")
         try:
             with os.fdopen(temp_file_fd, mode="w") as temp_file:
                 while True:
@@ -100,7 +112,7 @@ def writer(destination_file: str):
                     temp_file.write(f"{line}\n")
         except GeneratorExit:
             shutil.move(temp_file_path, destination_file)
-            print (f"Temporary file moved to: {destination_file}")
+            print(f"Temporary file moved to: {destination_file}")
 
 
 if __name__ == "__main__":
@@ -123,12 +135,11 @@ if __name__ == "__main__":
 
     for line in header_generator(flag_source_url, flag_name_server, flag_email_address):
         writer.send(line)
-    
-    print (f"Downloading and processing file at: {flag_source_url}")
+
+    print(f"Downloading and processing file at: {flag_source_url}")
     with request.urlopen(flag_source_url) as src_file:
         for line in src_file:
             extract_domain_name.send(line.decode())
 
     for i in commands:
         i.close()
-    
