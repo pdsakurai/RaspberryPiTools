2be6510ea9722d8028a85cabd00b71412c8a16b0
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2023-04-27 11:44:19 +0800

    New: Monitor interface using crontab

---
+++ monitor_interface.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+while [[ "$1" =~ ^- ]]; do case $1 in
+  -w | --wireguard )
+    is_wireguard=1
+    shift
+    ;;
+  -I | --interface )
+    shift
+    interface="$1"
+    shift
+    ;;
+  -p | --peer )
+    shift
+    peer_ip_address="$1"
+    shift
+    ;;
+  *)
+    echo "Unknown option: $1"
+    exit 1
+esac; done
+
+[ -n "$1" ] && echo "Unexpected arguments: $1" && exit 1
+
+[ ${interface:?Missing: Interface name} ]
+[ ${peer_ip_address:?Missing: Peer IP address} ]
+
+function is_peer_alive() {
+    ping -c 1 -W 1 -q -I $interface $peer_ip_address 2&> /dev/null
+}
+exit 0
+function is_wireguard() {
+    [ -n $is_wireguard ] || return 1
+    command -v wg-quick 2&> /dev/null && return 0
+    return 1
+}
+
+is_peer_alive || \
+if is_wireguard ; then
+    wg-quick down $interface
+    wg-quick up $interface
+else
+    ifup $interface
+fi
