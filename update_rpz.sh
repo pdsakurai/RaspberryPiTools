59e7c874fd1f1505eba98fbb2707bc1424b76721
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2024-08-17 12:44:29 +0800

    Changed: Check if there's diff before soft resetting

--- update_rpz.sh
+++ update_rpz.sh
@@ -59,9 +59,10 @@ readonly RPZ_DIR="$GITHUB_DIR/response_policy_zones"
 cd "$RPZ_DIR"
 git restore .
 git pull --rebase
-git reset --soft HEAD~
 update_rpz_files
 are_there_changes && {
+    git commit -a --message "${1:-Updated by cron.daily}"
+    git reset --soft HEAD~~
     git commit -a --message "${1:-Updated by cron.daily}"
     git push --force
 }
