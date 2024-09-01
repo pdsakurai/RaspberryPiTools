621439b87db3583c3a43b1333e481a64e656df5c
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2024-09-01 19:43:52 +0800

    Changed: Shorten TTLs

--- rpz_optimizer.py
+++ rpz_optimizer.py
@@ -85,11 +85,11 @@ def header_generator(
 
     to_seconds = lambda **kwargs: int(timedelta(**kwargs).total_seconds())
     time_to_ = {
-        "expire SOA": to_seconds(hours=6),
-        "refresh": to_seconds(hours=12),
+        "expire SOA": to_seconds(hours=2),
+        "refresh": to_seconds(hours=1),
         "retry": to_seconds(minutes=1),
         "expire": to_seconds(days=30),
-        "expire NXDOMAIN cache": to_seconds(hours=6),
+        "expire NXDOMAIN cache": to_seconds(hours=2),
     }
 
     yield ""
